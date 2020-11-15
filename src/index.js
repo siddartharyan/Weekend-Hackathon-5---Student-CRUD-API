7c52680416c294f03034688a9dba7d73eba04cf8
unknown <siddartharyan180@gmail.com>
2020-11-16 01:02:45 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -71,41 +71,58 @@ app.post('/api/student', (req, res) => {
 })
 
 app.put('/api/student/:id', (req, res) => {
-    let headers = req.body;
-    let changed = [];
-    Object.keys(headers).forEach((key1) => {
-        if (key1 === 'name' || key1 === 'currentclass' || key1 === 'division') {
-            if (key1 === 'currentclass') {
-                changed.push('currentClass');
-            } else {
-                changed.push(key1);
-            }
-        }
-    })
-    const id = Number(req.params.id);
-    let index = -1;
-    let obj = undefined;
-    for (let i = 0; i < data.length; i++) {
-        let did = Number(data[i].id);
-        if (did === id) {
-            index = i;
-            obj = data[i];
-            break;
-        }
-    }
-    if (index === -1) {
+    const id = req.params.id;
+    if (isNaN(id)) {
         res.status(400).json({});
         return;
     }
-    let updated = {};
-    for (let i = 0; i < changed.length; i++) {
-        obj[changed[i]] = (changed[i] === 'currentClass') ? Number(headers[changed[i].tolowerCase()]) : headers[changed[i].tolowerCase()];
-        updated[changed[i]] = (changed[i] === 'currentClass') ? Number(headers[changed[i].tolowerCase()]) : headers[changed[i].tolowerCase()];
+    let updData = req.body;
+    if (updData) {
+        let params = [];
+        for (let i = 0; i < updData.length; i++) {
+            if (updData[i] === 'name' || updData[i] === 'currentClass' || updData[i] === 'division') {
+                params.push(updData);
+            }
+        }
+        if (params.length === 0) {
+            res.status(400).json({});
+            return;
+        }
+        let index = -1;
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].id === id) {
+                index = i;
+                break;
+            }
+        }
+        if (index === -1 && params.length === 3) {
+            let id = (data.length !== 0) ? data[data.length - 1].id : -1;
+            let { name, currentClass, division } = updData;
+            id = (id !== 0) ? id + 1 : 0;
+            const obj = {
+                id: id,
+                name: name,
+                currentClass: Number(currentClass),
+                division: division
+            }
+            data.push(obj);
+            res.json();
+            return;
+        }
+        let obj = data[index];
+        for (let i = 0; i < params.length; i++) {
+            if (params[i] === 'currentClass') {
+                obj[params[i]] = Number(updData['currentClass']);
+            } else {
+                obj[params[i]] = updData[params[i]];
+            }
+        }
+        data[index] = obj;
+        res.json();
+    } else {
+        res.status(400).json({});
+        return;
     }
-    let arr = [...data];
-    arr[index] = obj;
-    data = arr;
-    res.json();
 
 })
 
