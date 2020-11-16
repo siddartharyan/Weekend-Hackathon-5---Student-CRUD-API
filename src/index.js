6b43004beefb7ceeebedc4a23d7b18f82e96a95a
unknown <siddartharyan180@gmail.com>
2020-11-16 14:49:10 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
 var data = require('./InitialData.js');
+let no = data.length;
 const port = 8080
 app.use(express.urlencoded());
 
@@ -70,64 +71,42 @@ app.post('/api/student', (req, res) => {
 })
 
 app.put('/api/student/:id', (req, res) => {
-    const id = req.params.id;
-    const data = req.body;
-    if (data) {
-        if (Object.is(parseInt(id), NaN)) {
-            res.sendStatus(400);
-        } else {
-            let found = false;
-            let validKey = ["name", "currentClass", "division"];
-            for (let i = 0; i < Object.keys(data).length; i++) {
-                found = validKey.includes(Object.keys(data)[i]);
-            }
-            if (!found) {
-                res.sendStatus(400);
-                return;
-            } else {
-                let newStudents = [...data];
-                let idFound = false;
-                newStudents = newStudents.map((student) => {
-                    if (student.id === Number(id)) {
-                        idFound = true;
-                        let newS = {...student }
-                        for (let i = 0; i < Object.keys(data).length; i++) {
-                            newS[Object.keys(data)[i]] = Object.keys(data)[i] === "currentClass" ? Number(data[Object.keys(data)[i]]) : data[Object.keys(data)[i]];
-                        }
-                        return newS;
-                    } else {
-                        return student
-                    }
-                })
-                if (!idFound) {
-                    if (Object.keys(data).length === 3) {
-                        const lastId = data.length !== 0 ? data[data.length - 1].id : -1;
-                        const { name, currentClass, division } = data;
-                        lastId = lastId !== 0 ? lastId + 1 : 0;
-                        const newStudent = {
-                            id: lastId,
-                            name: name,
-                            currentClass: Number(currentClass),
-                            division: division
-
-                        }
-
-                        data.push(newStudent);
-                        res.json()
-                    } else {
-                        res.sendStatus(400)
-                    }
-                } else {
-                    data = []
-                    data = [...newStudents]
-                    res.json();
-                }
-            }
-        }
-    } else {
-        res.sendStatus(400);
+    const id = Number(req.params.id);
+    if (isNaN(id)) {
+        res.status(400).json({});
         return;
     }
+    const header = req.body;
+    let update = {};
+    Object.keys(header).forEach((key1) => {
+        if (key1 === 'name' || key1 === 'currentClass' || key1 === 'division') {
+            if (key1 === 'currentClass') {
+                update[key1] = Number(header[key1]);
+            } else {
+                update[key1] = header[key1];
+            }
+        }
+    })
+    if (update.length === 0) {
+        res.json({});
+        return;
+    }
+    let index = -1;
+    let obj = undefined;
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].id === id) {
+            obj = data[i];
+            index = i;
+            break;
+        }
+    }
+    if (index === -1) {
+        res.status(400).json();
+        return;
+    }
+    obj = {...obj, ...update };
+    data[index] = obj;
+    res.json();
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
